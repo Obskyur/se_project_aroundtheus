4770f08c49ad068cee76800d4fa146859a2fa736
Tristan Miller <parkourman.miller@gmail.com>
2023-05-26 13:02:43 -0700

    cards render from js

--- scripts/index.js
+++ scripts/index.js
@@ -1,48 +1,3 @@
-/*══════════════╕
- │ DOM ELEMENTS │
- ╘══════════════*/
-const editButton = document.querySelector(".profile__edit-button");
-const profileName = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
-const modal = document.querySelector(".modal");
-const modalForm = modal.querySelector(".modal__form");
-const modalCloseButton = modal.querySelector(".modal__close");
-const modalNameInput = modal.querySelector("#modalName");
-const modalDescriptionInput = modal.querySelector("#modalDescription");
-
-/*════════════════╕
- │ EVENT HANDLERS │
- ╘════════════════*/
-function modalOpen() {
-  modalNameInput.value = profileName.textContent;
-  modalDescriptionInput.value = profileDescription.textContent;
-  modal.classList.add("modal_opened")
- }
-function modalClose() {
-  modal.classList.remove("modal_opened");
-}
-function handleProfileSubmit(evt) {
-  evt.preventDefault();
-  profileName.textContent = modalNameInput.value;
-  profileDescription.textContent = modalDescriptionInput.value;
-  modalClose();
-}
-
-/*════════════════╕
- │ PAGE FUNCTIONS │
- ╘════════════════*/
-editButton.addEventListener("click", modalOpen);
-
-/*═════════════════╕
- │ MODAL FUNCTIONS │
- ╘═════════════════*/
-modalCloseButton.addEventListener("click", modalClose);
-modalForm.addEventListener("submit", handleProfileSubmit);
-
-/*════════════════╕
- │ CARD FUNCTIONS │
- ╘════════════════*/
-
 /*═══════╕
  │ CARDS │
  ╘═══════*/
@@ -73,3 +28,71 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
   },
 ];
+
+/*══════════════╕
+ │ DOM ELEMENTS │
+ ╘══════════════*/
+const editButton = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const modal = document.querySelector(".modal");
+const modalForm = modal.querySelector(".modal__form");
+const modalCloseButton = modal.querySelector(".modal__close");
+const modalNameInput = modal.querySelector("#modalName");
+const modalDescriptionInput = modal.querySelector("#modalDescription");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListElement = document.querySelector(".cards__list");
+
+/*════════════════╕
+ │ EVENT HANDLERS │
+ ╘════════════════*/
+function modalOpen() {
+  modalNameInput.value = profileName.textContent;
+  modalDescriptionInput.value = profileDescription.textContent;
+  modal.classList.add("modal_opened");
+}
+function modalClose() {
+  modal.classList.remove("modal_opened");
+}
+function handleProfileSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = modalNameInput.value;
+  profileDescription.textContent = modalDescriptionInput.value;
+  modalClose();
+}
+
+/*════════════════╕
+ │ PAGE FUNCTIONS │
+ ╘════════════════*/
+editButton.addEventListener("click", modalOpen);
+
+/*═════════════════╕
+ │ MODAL FUNCTIONS │
+ ╘═════════════════*/
+modalCloseButton.addEventListener("click", modalClose);
+modalForm.addEventListener("submit", handleProfileSubmit);
+
+/*════════════════╕
+ │ CARD FUNCTIONS │
+ ╘════════════════*/
+
+// Add a card to HTML
+initialCards.forEach((cardData) => {
+  cardListElement.append(getCardElement(cardData));
+});
+
+// Given cardData, return raw HTML for card
+function getCardElement(cardData) {
+  // Clone Template
+  const cardElement = cardTemplate.cloneNode(true);
+  // Store Values
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardDescriptionElement = cardElement.querySelector(".card__title");
+  // Set Values for Card Instance
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
+  cardDescriptionElement.textContent = cardData.name;
+  // Return this 'card'
+  return cardElement;
+}
